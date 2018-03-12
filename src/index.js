2eccd41fdcf39fe85805e94a6eb4380378420dcd
vrcms <mslxd@163.com>
2018-03-12 17:35:05 +0800

    0312 17点

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import { app, BrowserWindow } from 'electron';
 
+var currentwin_width,currentwin_height;
+
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
@@ -13,7 +15,7 @@ let mainWindow;
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1100,
     height: 600,
     Frame:false
     
@@ -28,6 +30,14 @@ const createWindow = () => {
   //隐藏默认的菜单
   mainWindow.setMenu(null);
 
+  mainWindow.getBounds = function () {
+    var pos = this.getPosition();
+    var dimension = this.getSize();
+    return { x: pos[0], y: pos[1], width: dimension[0], height: dimension[1] };
+};
+
+  mainWindow.on('resize',updateReply);
+
   // Emitted when the window is closed.
   mainWindow.on('closed', () => {
     // Dereference the window object, usually you would store windows
@@ -35,6 +45,15 @@ const createWindow = () => {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
+
+  function updateReply(){
+    var testvar = mainWindow.getBounds();
+    console.log(testvar);
+    // var allcontainer = document.getElementById('allcontainer');
+    // allcontainer.style.width = currentwin_width+'px';
+    // allcontainer.style.height = currentwin_height+'px';
+  }
+
 };
 
 // This method will be called when Electron has finished
